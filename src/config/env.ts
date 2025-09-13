2e9d3048370f07dfd9aaafe1eb82c2f5963f4bec
Gagego Maciel <gagego.maciel@gmail.com>
2025-09-13 19:23:15 -0300

    chore: Production config

--- src/config/env.ts
+++ src/config/env.ts
@@ -9,7 +9,8 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   DATABASE_URL: z.string(),
-  PORT: z.preprocess((val) => Number(val), z.number().default(3333)),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']).default('sqlite'),
+  PORT: z.coerce.number().default(3333),
   HOST: z.string().default('0.0.0.0'),
 })
 
